009178db5f3f85b43967f5efe0b7b3d55c8e2180
Stewart Anderson <stewie1570@gmail.com>
2016-01-29 14:12:04 -0700

    Small refactoring.

--- src/parsing/index.js
+++ src/parsing/index.js
@@ -14,22 +14,9 @@ export class BowlingScoreCardParser {
                 };
             })
             .value();
-            
-        if(frames.length > 10)
-            throw new Error('Can\'t have more than 10 frames.');
-            
-        if(_(frames).take(9).some(frame => frame.rolls.length > 2))
-            throw new Error('Can\'t have more than 2 rolls per frame.');
-            
-        if(_(frames).take(9).some(frame => _(frame.rolls).sum() > 10))
-            throw new Error('Can\'t have a frame total more than 10 before the 10th frame.');
-            
-        if(_(frames).last().rolls.length > 3)
-            throw new Error('Can\'t have more than three rolls in 10th frame.');
-            
-        if(_(_(frames).last().rolls).sum() > 30)
-            throw new Error('10th frame cant have a frame total more than 30.');
-            
+
+        this._validateLengthsAndTotalsIn({ frames });
+
         return frames;
     }
 
@@ -45,5 +32,22 @@ export class BowlingScoreCardParser {
         };
 
         return _(frameString.split(',')).map(toIntegerRoll).value()
+    }
+
+    _validateLengthsAndTotalsIn({frames}) {
+        if (frames.length > 10)
+            throw new Error('Can\'t have more than 10 frames.');
+
+        if (_(frames).take(9).some(frame => frame.rolls.length > 2))
+            throw new Error('Can\'t have more than 2 rolls per frame.');
+
+        if (_(frames).take(9).some(frame => _(frame.rolls).sum() > 10))
+            throw new Error('Can\'t have a frame total more than 10 before the 10th frame.');
+
+        if (_(frames).last().rolls.length > 3)
+            throw new Error('Can\'t have more than three rolls in 10th frame.');
+
+        if (_(_(frames).last().rolls).sum() > 30)
+            throw new Error('10th frame cant have a frame total more than 30.');
     }
 }
