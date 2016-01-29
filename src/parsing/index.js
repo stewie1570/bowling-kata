3b9c974732a7c49f4cbe6255cf401a24be19c77d
Stewart Anderson <stewie1570@gmail.com>
2016-01-29 02:16:22 -0700

    Started on parser validation.

--- src/parsing/index.js
+++ src/parsing/index.js
@@ -2,14 +2,26 @@ import _ from 'lodash';
 
 export class BowlingScoreCardParser {
     parse({delimitedScores}) {
+        if (delimitedScores.indexOf('|') < 0)
+            throw new Error(`No pipe delimited frames in "${delimitedScores}".`);
+
         var frames = delimitedScores.split('|');
 
         return _(frames)
             .map(frame => {
                 return {
-                    rolls: _(frame.split(',')).map(roll => parseInt(roll)).value()
+                    rolls: this._rollsFrom({ commaDelimitedString: frame })
                 };
             })
             .value();
+    }
+
+    _rollsFrom({commaDelimitedString}) {
+        if(commaDelimitedString.indexOf(',') < 0)
+            throw new Error(`No comma delimited rolls in "${commaDelimitedString}".`);
+        
+        return _(commaDelimitedString.split(','))
+            .map(roll => parseInt(roll))
+            .value()
     }
 }
--- test/parsing/scoreCard.spec.js
+++ test/parsing/scoreCard.spec.js
@@ -1,16 +1,44 @@
 import { BowlingScoreCardParser } from '../../src/parsing';
 
-describe('Score Card Parsing', () => {
+describe('Score Card Parser', () => {
     var parser;
 
     beforeEach(() => parser = new BowlingScoreCardParser());
 
-    it('should parse comma/pipe delimited rolls and frames', () => {
-        var results = parser.parse({ delimitedScores: '1,2|2,5|8,1' });
-        expect(results).to.deep.equal([
-            { rolls: [1, 2] },
-            { rolls: [2, 5] },
-            { rolls: [8, 1] }
-        ]);
+    describe('Parsing', () => {
+        it('should parse comma/pipe delimited rolls and frames', () => {
+            var results = parser.parse({ delimitedScores: '1,2|2,5|8,1' });
+            expect(results).to.deep.equal([
+                { rolls: [1, 2] },
+                { rolls: [2, 5] },
+                { rolls: [8, 1] }
+            ]);
+        });
+    });
+    
+    describe('Error Handling', () => {
+        it('should throw when no frames', () => {
+            var exceptionMessage = '';
+            try{
+                parser.parse({delimitedScores: 'blah'});
+            }
+            catch(ex){
+                exceptionMessage = ex.message;
+            }
+            
+            expect(exceptionMessage).to.equal('No pipe delimited frames in "blah".');
+        });
+        
+        it('should throw when no rolls', () => {
+            var exceptionMessage = '';
+            try{
+                parser.parse({delimitedScores: 'blah|blah'});
+            }
+            catch(ex){
+                exceptionMessage = ex.message;
+            }
+            
+            expect(exceptionMessage).to.equal('No comma delimited rolls in "blah".');
+        });
     });
 });
