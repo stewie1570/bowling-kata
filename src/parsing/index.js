6d765adbefe25ece5eb0acebd155ed761b47ca0f
Stewart Anderson <stewie1570@gmail.com>
2016-01-29 12:59:52 -0700

    Error handling for non-integer rolls and more than 2 rolls per frame.

--- src/parsing/index.js
+++ src/parsing/index.js
@@ -7,21 +7,31 @@ export class BowlingScoreCardParser {
 
         var frameStrings = delimitedScores.split('|');
 
-        return _(frameStrings)
+        var frames = _(frameStrings)
             .map(frameString => {
                 return {
-                    rolls: this._rollsFrom({ commaDelimitedString: frameString })
+                    rolls: this._rollsFrom({ frameString })
                 };
             })
             .value();
+            
+        if(_(frames).some(frame => frame.rolls.length > 2))
+            throw new Error(`Can't have more than 2 rolls per frame.`);
+            
+        return frames;
     }
 
-    _rollsFrom({commaDelimitedString}) {
-        if(commaDelimitedString.indexOf(',') < 0)
-            throw new Error(`No comma delimited rolls in "${commaDelimitedString}".`);
-        
-        return _(commaDelimitedString.split(','))
-            .map(roll => parseInt(roll))
-            .value()
+    _rollsFrom({frameString}) {
+        if (frameString.indexOf(',') < 0)
+            throw new Error(`No comma delimited rolls in "${frameString}".`);
+
+        var toIntegerRoll = stringRoll => {
+            var val = parseInt(stringRoll);
+            if (!val)
+                throw new Error(`Rolls need to be integers. "${stringRoll}" is not.`);
+            return val;
+        };
+
+        return _(frameString.split(',')).map(toIntegerRoll).value()
     }
 }
--- test/parsing/scoreCard.spec.js
+++ test/parsing/scoreCard.spec.js
@@ -40,5 +40,29 @@ describe('Score Card Parser', () => {
             
             expect(exceptionMessage).to.equal('No comma delimited rolls in "blah".');
         });
+        
+        it('should throw when rolls are not integers', () => {
+            var exceptionMessage = '';
+            try{
+                parser.parse({delimitedScores: 'blah,blah|blah,blah'});
+            }
+            catch(ex){
+                exceptionMessage = ex.message;
+            }
+            
+            expect(exceptionMessage).to.equal('Rolls need to be integers. "blah" is not.');
+        });
+        
+        it('cant have more than two rolls per frame', () => {
+            var exceptionMessage = '';
+            try{
+                parser.parse({delimitedScores: '1,2|3,4,5|4,3|1,2,3'});
+            }
+            catch(ex){
+                exceptionMessage = ex.message;
+            }
+            
+            expect(exceptionMessage).to.equal('Can\'t have more than 2 rolls per frame.');
+        });
     });
 });
