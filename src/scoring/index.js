ad58efbf80f85afe0d1aa0ee67e01ab322677208
Stewart Anderson <stewie1570@gmail.com>
2016-01-30 11:34:28 -0700

    BowlingScoreBoard is working.

--- src/scoring/index.js
+++ src/scoring/index.js
@@ -1,17 +1,30 @@
 import _ from 'lodash';
 
 export class BowlingScoreBoard {
-	scoredGameFrom({frames}) {
-		return {
-			total: _(frames).flatMap(frame => frame.rolls).sum(),
-			frames: _(frames)
-				.map(frame => {
-					return {
-						rolls: frame.rolls,
-						total: _(frame.rolls).sum()
-					};
-				})
-				.value()
-		};
-	}
+    scoredGameFrom({frames}) {
+        var totalledFrames = _(frames)
+            .map(frame => {
+                return {
+                    rolls: frame.rolls,
+                    total: _(frame.rolls).sum()
+                };
+            })
+            .value();
+
+        var bonusTotalledFrames = _(totalledFrames)
+            .zipWith(_(totalledFrames).takeRight(totalledFrames.length - 1).value(), (left, right) => {
+                return {
+                    rolls: left.rolls,
+                    total: left.total + (left.total == 10
+                        ? left.rolls.length == 1 ? right.total : _(right.rolls).first()
+                        : 0)
+                }
+            })
+            .value();
+        
+        return {
+            total: _(bonusTotalledFrames).map(frame => frame.total).sum(),
+            frames: bonusTotalledFrames
+        };
+    }
 }
--- test/scoring/bowlingScorer.spec.js
+++ test/scoring/bowlingScorer.spec.js
@@ -1,27 +1,64 @@
 import { BowlingScoreBoard } from '../../src/scoring';
+import _ from 'lodash';
 
 describe('Scoring', () => {
-	var scorer;
+    var scorer;
 
-	beforeEach(() => scorer = new BowlingScoreBoard())
+    beforeEach(() => scorer = new BowlingScoreBoard())
 
-	it('should sub-total all frames and grand total the game', () => {
-		var scoreBoard = scorer
-			.scoredGameFrom({
-				frames: [
-					{ rolls: [1, 2] },
-					{ rolls: [2, 5] },
-					{ rolls: [3, 1] }
-				]
-			});
-		
-		expect(scoreBoard).to.deep.equal({
-			total: 14,
-			frames: [
+    it('should sub-total all frames and grand total the game', () => {
+        var scoreBoard = scorer
+            .scoredGameFrom({
+                frames: [
+                    { rolls: [1, 2] },
+                    { rolls: [2, 5] },
+                    { rolls: [3, 1] }
+                ]
+            });
+
+        expect(scoreBoard).to.deep.equal({
+            total: 14,
+            frames: [
                 { rolls: [1, 2], total: 3 },
                 { rolls: [2, 5], total: 7 },
                 { rolls: [3, 1], total: 4 }
             ]
-		});
-	});
+        });
+    });
+
+    it('should add bonus of the next roll on frames scoring a spare', () => {
+        var scoreBoard = scorer
+            .scoredGameFrom({
+                frames: [
+                    { rolls: [1, 9] },
+                    { rolls: [2, 5] },
+                    { rolls: [3, 1] }
+                ]
+            });
+
+        expect(scoreBoard.total).to.equal(23);
+        expect(scoreBoard.frames).to.deep.equal([
+            { rolls: [1, 9], total: 12 },
+            { rolls: [2, 5], total: 7 },
+            { rolls: [3, 1], total: 4 }
+        ]);
+    });
+    
+    it('should add bonus of the next two rolls on frames scoring a strike', () => {
+        var scoreBoard = scorer
+            .scoredGameFrom({
+                frames: [
+                    { rolls: [1, 5] },
+                    { rolls: [10] },
+                    { rolls: [3, 1] }
+                ]
+            });
+
+        expect(scoreBoard.total).to.equal(24);
+        expect(scoreBoard.frames).to.deep.equal([
+            { rolls: [1, 5], total: 6 },
+            { rolls: [10], total: 14 },
+            { rolls: [3, 1], total: 4 }
+        ]);
+    });
 });
