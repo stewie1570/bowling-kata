d2558087a24e22300c42875c815c9d0615ad2200
Stewart Anderson <stewie1570@gmail.com>
2016-02-01 20:05:58 -0700

    View to align totals to the right.

--- src/ui/view.js
+++ src/ui/view.js
@@ -1,21 +1,22 @@
 import _ from 'lodash';
 
-var minWidth = (str, pad, length) => str.length > length ? str : str + Array(length - str.length).join(pad);
+var padRight = (str, pad, length) => str.length > length ? str : str + Array(length - str.length).join(pad);
+var padLeft = (str, pad, length) => str.length > length ? str : Array(length - str.length).join(pad) + str;
 
 export var view = {
 	render: gameViewModel => {
         var border = "=====================================================================================";
 		console.log(border);
 		console.log(`|${
-			minWidth(_(gameViewModel.frames)
+			padRight(_(gameViewModel.frames)
 				.flatMap(frame => frame.rolls)
 				.value()
 				.join(' | '), ' ', border.length - 1)
 		}|`);
 		console.log(`|${
-			minWidth(_(gameViewModel.frames)
+			padRight(_(gameViewModel.frames)
 				// .map((frame, index) => `       ${frame.total || ''}`.slice(index === 9 ? -7 : -5))
-                .map((frame, index) => minWidth(`${frame.total || ''}`, ' ', index === 9 ? 11 : 6))
+                .map((frame, index) => padLeft(`${frame.total || ''}`, ' ', index === 9 ? 11 : 6))
 				.value()
 				.join(' | '), ' ', border.length - 1)
 		}|`);
