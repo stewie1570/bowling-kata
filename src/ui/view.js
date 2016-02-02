8f3fb4fa18d6ad8c14567c65d6db740740c08482
Stewart Anderson <stewie1570@gmail.com>
2016-02-01 20:02:51 -0700

    Minor view adjustments.

--- src/ui/view.js
+++ src/ui/view.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-var minWidth = (str, pad, length) => str + Array(length - str.length).join(pad);
+var minWidth = (str, pad, length) => str.length > length ? str : str + Array(length - str.length).join(pad);
 
 export var view = {
 	render: gameViewModel => {
@@ -14,7 +14,8 @@ export var view = {
 		}|`);
 		console.log(`|${
 			minWidth(_(gameViewModel.frames)
-				.map((frame, index) => `       ${frame.total || ''}`.slice(index === 9 ? -7 : -5))
+				// .map((frame, index) => `       ${frame.total || ''}`.slice(index === 9 ? -7 : -5))
+                .map((frame, index) => minWidth(`${frame.total || ''}`, ' ', index === 9 ? 11 : 6))
 				.value()
 				.join(' | '), ' ', border.length - 1)
 		}|`);
