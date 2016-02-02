996c99b142085aa63c5b45d3f0835ddf724f2646
Stewart Anderson <stewie1570@gmail.com>
2016-02-02 13:15:01 -0700

    Refactored.

--- src/ui/controller.js
+++ src/ui/controller.js
@@ -26,7 +26,7 @@ export class BowlingGameController {
 					.length;
 
 				return {
-					rolls: this._displayRollsIn({frame}),
+					rolls: this._displayRollsIn({ frame }),
 					total: frameIndex === 9 ? game.total : this._isMissingBonusRolls({ frame, futureRollsCount })
 						? undefined
 						: _(game.frames)
@@ -40,13 +40,17 @@ export class BowlingGameController {
 
 	_displayRollsIn({frame}) {
 		var rolls = _(frame.rolls)
-			.map((roll, rollIndex) => rollIndex % 2 === 0 || (rollIndex > 0 && frame.rolls[rollIndex - 1] === 10)
+			.map((roll, rollIndex) => this._isStrikePossibleFor({ rollIndex, previousRoll: frame.rolls[rollIndex - 1] })
 				? (roll === 10 ? 'X' : roll.toString())
 				: (_(frame.rolls).take(rollIndex + 1).sum() === 10 ? '/' : roll.toString()))
 			.value();
 		var strikeFormattedRolls = rolls.length === 1 && rolls[0] === 'X' ? [' ', 'X'] : rolls;
-		
+
 		return strikeFormattedRolls;
+	}
+
+	_isStrikePossibleFor({rollIndex, previousRoll}) {
+		return rollIndex % 2 === 0 || (rollIndex > 0 && previousRoll === 10);
 	}
 
 	_isMissingBonusRolls({frame, futureRollsCount}) {
