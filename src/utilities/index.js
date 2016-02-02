310330271f41d98d576784ef3547defde31becba
Stewart Anderson <stewie1570@gmail.com>
2016-02-02 17:22:57 +0000

    Minor refactor.

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
 
 export function mapRight(array, mapper) {
-    return _(array).map((element, index) => mapper(element, _(array).takeRight(array.length - index - 1)));
+    return _(array).map((element, index) => mapper(element, _(array).takeRight(array.length - index - 1).value()));
 }
