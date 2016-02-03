558d1922ce9531ae9c175ad27b07e4e87cdb00ae
Stewart Anderson <stewie1570@gmail.com>
2016-02-03 13:11:51 -0700

    Refactored.

--- src/ui/controller.js
+++ src/ui/controller.js
@@ -57,17 +57,21 @@ export class BowlingGameController {
 
 	_displayRollsIn({frame, frameIndex}) {
 		var emptyRolls = count => _(Array(count)).map(x => ' ').value();
+		var strikeFormattedRolls = this._strikeFormattedRollsFrom({frame, frameIndex});
+		var expectedRollCountForFrame = frameIndex < 9 ? 2 : 3;
+
+		return strikeFormattedRolls.length < expectedRollCountForFrame
+			? strikeFormattedRolls.concat(emptyRolls(expectedRollCountForFrame - strikeFormattedRolls.length))
+			: strikeFormattedRolls;
+	}
+	
+	_strikeFormattedRollsFrom({frame, frameIndex}){
 		var rolls = _(frame.rolls)
 			.map((roll, rollIndex) => this._isStrikePossibleFor({ rollIndex, previousRoll: frame.rolls[rollIndex - 1] })
 				? (roll === 10 ? 'X' : roll.toString())
 				: (_(frame.rolls).take(rollIndex + 1).sum() === 10 ? '/' : roll.toString()))
 			.value();
-		var strikeFormattedRolls = frameIndex < 9 && rolls[0] === 'X' ? [' ', 'X'] : rolls;
-		var expectedRolls = frameIndex < 9 ? 2 : 3;
-
-		return strikeFormattedRolls.length < expectedRolls
-			? strikeFormattedRolls.concat(emptyRolls(expectedRolls - strikeFormattedRolls.length))
-			: strikeFormattedRolls;
+		return frameIndex < 9 && rolls[0] === 'X' ? [' ', 'X'] : rolls;
 	}
 
 	_isStrikePossibleFor({rollIndex, previousRoll}) {
