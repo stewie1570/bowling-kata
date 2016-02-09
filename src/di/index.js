ebb7ad564ec761331906d8749cced077215931f4
Stewart Anderson <stewie1570@gmail.com>
2016-02-09 14:55:30 -0700

    Moved dependency bindings from main to './di'.

---
+++ src/di/index.js
@@ -0,0 +1,13 @@
+import {DelimitedStringBowlingScoreCardParser} from '../parsing';
+import {BowlingScoreBoard} from '../scoring';
+import {view} from '../ui/view';
+import {Ioc} from 'javascript-ioc';
+import {GameProvider} from '../providers/gameProvider';
+
+export var ioc = new Ioc();
+
+ioc.bind("view", { to: view });
+ioc.bind("scorer", { to: BowlingScoreBoard });
+ioc.bind("parser", { to: DelimitedStringBowlingScoreCardParser });
+ioc.bind("userInput", { toConstant: () => global.process.argv[2] });
+ioc.bind("gameProvider", { to: GameProvider });
--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
-import {DelimitedStringBowlingScoreCardParser} from './parsing';
-import {BowlingScoreBoard} from './scoring';
+import {ioc} from './di';
 import {BowlingGameController} from './ui/controller';
-import {view} from './ui/view';
-import {Ioc} from 'javascript-ioc';
-import {GameProvider} from './providers/gameProvider';
-
-var ioc = new Ioc();
-
-global.process = global.process || { argv: [] };
-
-ioc.bind("view", { to: view });
-ioc.bind("scorer", { to: BowlingScoreBoard });
-ioc.bind("parser", { to: DelimitedStringBowlingScoreCardParser });
-ioc.bind("userInput", { toConstant: () => global.process.argv[2] });
-ioc.bind("gameProvider", { to: GameProvider });
 
 ioc.get(BowlingGameController).showGame();
