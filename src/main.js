854615eddbdc19ebfd52e5222edacd64ba3e7db5
Stewart Anderson <stewie1570@gmail.com>
2016-03-23 11:58:08 -0600

    Output to include the dependency graph.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import {ioc} from './di';
 import {BowlingGameController} from './ui/controller';
 
 ioc.get(BowlingGameController).showGame();
+
+console.log(JSON.stringify(ioc.getDependencyGraphOf(BowlingGameController), null, '\t'));
